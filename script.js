280815acdc10182b8bd7bcdcdc2c0f5a21f7b72a
dev-salih <salihmu79@gmail.com>
2026-05-04 20:55:50 +0300

    generateQR function is declared

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ let imgBox = document.getElementById("imgBox");
 let qrImage = document.getElementById("qrImage");
 let qrText = document.getElementById("qrText");
 let btn = document.getElementById("btn");
+
+function generateQR() {
+    qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value; 
+}
