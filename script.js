5269a1fd6e0c83a44840f04700f54bde57d97b13
dev-salih <salihmu79@gmail.com>
2026-05-04 21:00:56 +0300

    btn.addEventListener is used

--- script.js
+++ script.js
@@ -6,3 +6,5 @@ let btn = document.getElementById("btn");
 function generateQR() {
     qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value; 
 }
+
+btn.addEventListener("click", generateQR);
