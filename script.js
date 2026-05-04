040a13f44a2fdbbd9a73ffa23eed8d0c688a65f1
dev-salih <salihmu79@gmail.com>
2026-05-04 20:51:25 +0300

    btn is selected

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 let imgBox = document.getElementById("imgBox");
 let qrImage = document.getElementById("qrImage");
 let qrText = document.getElementById("qrText");
+let btn = document.getElementById("btn");
