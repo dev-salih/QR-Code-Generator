0832899f312b298fff4689fe699bca35095901c2
dev-salih <salihmu79@gmail.com>
2026-05-04 21:31:58 +0300

    the new class show-img is added inside the function

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ let qrText = document.getElementById("qrText");
 let btn = document.getElementById("btn");
 
 function generateQR() {
-    qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value; 
+    qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+    imgBox.classList.add("show-img");
 }
 
 btn.addEventListener("click", generateQR);
